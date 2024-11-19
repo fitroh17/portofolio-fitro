23d8932895957ae34afdc158803fc9148c8874d0
fitroh17 <143379878+fitroh17@users.noreply.github.com>
2024-11-19 19:34:54 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//toggle & responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
